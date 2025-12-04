4a8d3ecff4f19004bc0dfcb6533c7a43c93bf393
shabnamswain <65340317+shabnamswain@users.noreply.github.com>
2025-12-05 01:43:33 +0530

    Configure GCS backend and update provider settings
    Added backend configuration for GCS and zone to provider.

--- providers.tf
+++ providers.tf
@@ -1,14 +1,41 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = "7.12.0"
     }
   }
 }
 
+terraform {
+    backend "gcs" { 
+      bucket  = "terraformstatecicdbucket"
+      prefix  = "vang"
+    }
+}
+
 provider "google" {
   project = "leafy-glyph-477712-p3"
   region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
+resource "google_project_service" "api_services" {
+  for_each = toset([
+    "compute.googleapis.com",
+    "run.googleapis.com",
+    "iam.googleapis.com",
+    "sqladmin.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "vpcaccess.googleapis.com",
+    "storage.googleapis.com"
+  ])
+  project = "leafy-glyph-477712-p3"
+  service = each.key
+}
+
+
+#workflow dispatch
+# SA
+# WIF
+# LINK SA
